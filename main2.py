610d351e4bf622606f1a87894f094fc181029e4d
Pooolg <59968996+Pooolg@users.noreply.github.com>
2021-06-19 16:03:48 +0500

    Update main2.py
    Added introduction log

--- main2.py
+++ main2.py
@@ -2,25 +2,45 @@
 import time
 import requests
 
-tg_token = "token here"
+tg_token = "token"
 channel = "@example"
 
-while True :
+def response(r):
+    if r['ok'] is True:
+        print('[' + time.ctime(time.time()) + ']', 'OK:', r['ok'], ';#' + str(r['result']['message_id']))
+    else:
+        print('[' + time.ctime(time.time()) + ']', 'OK:', r['ok'], '; Error:',r['error_code'],'\n',r['description'],)
+
+while True:
     def postnekopic():
-        picURL = requests.get(f"https://nekos.life/api/v2/img/neko").json()['url']
-        r = requests.get("https://api.telegram.org/bot"+tg_token+"/sendPhoto?chat_id="+channel+"&photo="+picURL+"&caption=%23neko" )
+        picURL = requests.get("https://nekos.life/api/v2/img/neko").json()['url']
+        r = requests.get(
+            "https://api.telegram.org/bot" + tg_token + "/sendPhoto?chat_id=" + channel + "&photo=" + picURL + "&caption=%23neko").json()
+        response(r)
+
     def postbakapic():
-        picURL = requests.get(f"https://nekos.life/api/v2/img/baka").json()['url']
-        r = requests.get("https://api.telegram.org/bot"+tg_token+"/sendPhoto?chat_id="+channel+"&photo=" + picURL + "&caption=%23baka")
+        picURL = requests.get("https://nekos.life/api/v2/img/baka").json()['url']
+        r = requests.get(
+            "https://api.telegram.org/bot" + tg_token + "/sendPhoto?chat_id=" + channel + "&photo=" + picURL + "&caption=%23baka").json()
+        response(r)
+
     def postfoxgirlpic():
-        picURL = requests.get(f"https://nekos.life/api/v2/img/fox_girl").json()['url']
-        r = requests.get("https://api.telegram.org/bot"+tg_token+"/sendPhoto?chat_id="+channel+"&photo=" + picURL + "&caption=%23fox_girl")
+        picURL = requests.get("https://nekos.life/api/v2/img/fox_girl").json()['url']
+        r = requests.get(
+            "https://api.telegram.org/bot" + tg_token + "/sendPhoto?chat_id=" + channel + "&photo=" + picURL + "&caption=%23fox_girl").json()
+        response(r)
+
     def postpokepic():
-        picURL = requests.get(f"https://nekos.life/api/v2/img/poke").json()['url']
-        r = requests.get("https://api.telegram.org/bot"+tg_token+"/sendPhoto?chat_id="+channel+"&photo=" + picURL + "&caption=%23poke")
+        picURL = requests.get("https://nekos.life/api/v2/img/poke").json()['url']
+        r = requests.get(
+            "https://api.telegram.org/bot" + tg_token + "/sendPhoto?chat_id=" + channel + "&photo=" + picURL + "&caption=%23poke").json()
+        response(r)
+
     def postkisspic():
-        picURL = requests.get(f"https://nekos.life/api/v2/img/kiss").json()['url']
-        r = requests.get("https://api.telegram.org/bot"+tg_token+"/sendPhoto?chat_id="+channel+"&photo=" + picURL + "&caption=%23kiss")
+        picURL = requests.get("https://nekos.life/api/v2/img/kiss").json()['url']
+        r = requests.get(
+            "https://api.telegram.org/bot" + tg_token + "/sendPhoto?chat_id=" + channel + "&photo=" + picURL + "&caption=%23kiss").json()
+        response(r)
 
     postnekopic()
     time.sleep(3600)
